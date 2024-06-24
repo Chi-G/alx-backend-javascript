6b6ac497cd44902abaea225fda19e742e8e55e8b
root <Chi-G>
2024-06-24 23:39:35 +0000

    new commit

--- 0x00-ES6_basic/.eslintrc.js
+++ 0x00-ES6_basic/.eslintrc.js
@@ -1,35 +1,25 @@
 module.exports = {
   env: {
-    browser: false,
-    es6: true,
-    jest: true,
+    browser: true,
+    es2021: true,
+    jest: true
   },
   extends: [
-    'airbnb-base',
-    'plugin:jest/all',
+    'eslint:recommended',
+    'plugin:react/recommended'
   ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
   parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: 'module'
   },
-  plugins: ['jest'],
+  plugins: [
+    'react'
+  ],
   rules: {
-    'no-console': 'off',
-    'no-shadow': 'off',
-    'no-restricted-syntax': [
-      'error',
-      'LabeledStatement',
-      'WithStatement',
-    ],
-  },
-  overrides:[
-    {
-      files: ['*.js'],
-      excludedFiles: 'babel.config.js',
-    }
-  ]
+    'react/prop-types': 'off'
+  }
 };
+
